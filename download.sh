30cc9a9d0c07b8d417065cb4313bb241cf958470
Jintao Meng <meng.jintao@gmail.com>
2019-05-07 15:34:14 +0800

    update download.sh and upload caffe2ncnn converter tool

--- download.sh
+++ download.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-
+git clone https://github.com/tencent/feathercnn
+git clone https://github.com/tencent/ncnn
+mv feathercnn feather
+cd feather/
+git checkout master
+cd ../ncnn
+git reset --hard d46c5989e1768caf59ad4b1ef2ba620573aa2685
